050d8353ab5cd575269d6cf604ceb4a95d8690e0
kleanthis kyriazakis <47750350+kkyriazakis@users.noreply.github.com>
2019-03-30 18:34:30 +0200

    myLDA

--- HW1/exercise1_3/myLDA.m
+++ HW1/exercise1_3/myLDA.m
@@ -6,40 +6,44 @@ function A = myLDA(Samples, Labels, NewDim)
        
     [NumSamples NumFeatures] = size(Samples);
 
-	A=zeros(NumFeatures,NewDim);
+	A = zeros(NumFeatures,NewDim);
     
-	
     NumLabels = length(Labels);
-    if(NumSamples ~= NumLabels) then
+    if(NumSamples ~= NumLabels)
         fprintf('\nNumber of Samples are not the same with the Number of Labels.\n\n');
         exit
     end
     Classes = unique(Labels);
-    NumClasses = length(Classes)  %The number of classes
+    NumClasses = length(Classes);  %The number of classes
 
-    %For each class i
-	%Find the necessary statistics
+    for i=1:NumClasses %Find the necessary statistics
+        
+        %Calculate the Class Prior Probability
+        P(i) = length(find(iris_labels == 0))/NumLabels;
+        
+        %Calculate the Class Mean 
+        mu(i,:) = mean(Samples( iris_labels == i-1 ,:),1);
+    end
     
-    %Calculate the Class Prior Probability
-	P(i)=
-    %Calculate the Class Mean 
-	mu(i,:)
     %Calculate the Within Class Scatter Matrix
-	Sw=
+    Sw = cov(Samples( iris_labels==0,:)) + cov(Samples( iris_labels==1,:)) + cov(Samples( iris_labels==2,:));
+        
     %Calculate the Global Mean
-	m0=
-
-  
+    m0 = mean(mean(Samples));
+    
     %Calculate the Between Class Scatter Matrix
-	Sb= 
+	Sb = length(find(iris_labels == 0))*(mu(1,:) - m0)*(mu(1,:) - m0)';
+    Sb = Sb + length(find(iris_labels == 1))*(mu(2,:) - m0)*(mu(2,:) - m0)';
+    Sb = Sb + length(find(iris_labels == 2))*(mu(3,:) - m0)*(mu(3,:) - m0)';
     
     %Eigen matrix EigMat=inv(Sw)*Sb
     EigMat = inv(Sw)*Sb;
     
     %Perform Eigendecomposition
-
+    
     
     %Select the NewDim eigenvectors corresponding to the top NewDim
     %eigenvalues (Assuming they are NewDim<=NumClasses-1)
-	%% You need to return the following variable correctly.
-	A=zeros(NumFeatures,NewDim);  % Return the LDA projection vectors
+	A = zeros(NumFeatures,NewDim);  % Return the LDA projection vectors
+    
+    
